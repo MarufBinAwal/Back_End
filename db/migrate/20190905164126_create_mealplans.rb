20f4f6f0c3fb82ea28a3cd2099644209e7c3e374
github username <github email address>
2019-09-05 17:02:31 -0500

    added tables in migration

--- db/migrate/20190905164126_create_mealplans.rb
+++ db/migrate/20190905164126_create_mealplans.rb
@@ -1,6 +1,8 @@
 class CreateMealplans < ActiveRecord::Migration[5.2]
   def change
     create_table :mealplans do |t|
+      t.integer :user_id
+      t.integer :macro_total
 
       t.timestamps
     end
--- db/migrate/20190905164146_create_recipies.rb
+++ db/migrate/20190905164146_create_recipies.rb
@@ -2,6 +2,17 @@ class CreateRecipies < ActiveRecord::Migration[5.2]
   def change
     create_table :recipies do |t|
 
+        t.text :name
+        t.string :img
+        t.string :ingredients
+        t.string :intructions
+        t.integer :calories
+        t.integer :fat
+        t.integer :carbohydrates
+        t.integer :protein
+        t.integer :fiber
+        t.integer :servings
+
       t.timestamps
     end
   end
